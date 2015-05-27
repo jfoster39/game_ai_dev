ae73e523989c0dbb94f4e0061a698f09b5694e5b
Ty Foster <jfoster39@gatech.edu>
2015-05-26 22:25:33 -0400

    moved gitignore file, build path network complete

--- homework1.5/mybuildpathnetwork.py
+++ homework1.5/mybuildpathnetwork.py
@@ -7,10 +7,30 @@ from core import *
 
 # Creates the pathnetwork as a list of lines between all pathnodes that are traversable by the agent.
 def myBuildPathNetwork(pathnodes, world, agent = None):
-	lines = []
-	### YOUR CODE GOES BELOW HERE ###
+    lines                   = []
+    pathNodesCopy           = copy.copy( pathnodes )
+    obstacleLines           = world.getLinesWithoutBorders()
+    obstaclePoints          = world.getPoints()
+    minCollisionDistance    = agent.radius * 2
+    ### YOUR CODE GOES BELOW HERE ###
 
+    # Find all lines between pathnodes which do not intersect obstacle.
+    for pathNode in pathnodes:
+        for pathNodeCopy in pathNodesCopy:
+            if ( cmp( pathNode, pathNodeCopy ) != 0 and
+                    rayTraceWorld( pathNode, pathNodeCopy, obstacleLines ) == None ):
 
+                # Calculate distance from nearest obstacle point to line.
+                distance = INFINITY
+                for point in obstaclePoints:
+                    minDistance = minimumDistance( (pathNode, pathNodeCopy), point )
+                    if minDistance < distance:
+                        distance = minDistance
 
-	### YOUR CODE GOES ABOVE HERE ###
-	return lines
+                # Add line to lines if min distance from point to line does not cause
+                # obstacle collision.
+                if distance > minCollisionDistance:
+                    lines.append( (pathNode, pathNodeCopy) )
+
+    ### YOUR CODE GOES ABOVE HERE ###
+    return lines
