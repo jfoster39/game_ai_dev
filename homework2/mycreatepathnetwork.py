5d469d2874f1b1faa8a50bad6b35103a18ebc6f4
Ty Foster <jfoster39@gatech.edu>
2015-06-03 19:11:41 -0400

    nav mesh initial commit

---
+++ homework2/mycreatepathnetwork.py
@@ -0,0 +1,18 @@
+import sys, pygame, math, numpy, random, time, copy, operator
+from pygame.locals import *
+
+from constants import *
+from utils import *
+from core import *
+
+# Creates a pathnode network that connects the midpoints of each navmesh together
+def myCreatePathNetwork(world, agent = None):
+	nodes = []
+	edges = []
+	polys = []
+	### YOUR CODE GOES BELOW HERE ###
+
+	### YOUR CODE GOES ABOVE HERE ###
+	return nodes, edges, polys
+
+	
